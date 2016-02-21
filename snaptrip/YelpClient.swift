77fef6d5a756bc820c4f169d8a3cddcfede52935
MrSATA <liuhanrick@163.com>
2016-02-21 11:54:54 -0600

    add yelpclient
    cannot run yelp so far

---
+++ snaptrip/YelpClient.swift
@@ -0,0 +1,92 @@
+//
+//  YelpClient.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 9/19/14.
+//  Changed by Guocheng Wei on 2/21/16
+//  Copyright (c) 2014 Timothy Lee. All rights reserved.
+//
+
+
+// Business : rating        number
+//            review_count  number
+
+// /v2/business/{id}        GET     Lookup business information by id.
+
+import UIKit
+import AFNetworking
+import BDBOAuth1Manager
+
+let yelpConsumerKey = "IDtK3KrZDyz4EtDgFAEdog"
+let yelpConsumerSecret = "8bRD6bJe7tFTrT8aCpQBpT-ulPk"
+let yelpToken = "KBL4zf_uAjgn2Dp_0UVC_fxQAZl5Dsbh"
+let yelpTokenSecret = "KhLEsrl8vo2SjBHF1BSK8feY5sw"
+
+enum YelpSortMode: Int {
+    case BestMatched = 0, Distance, HighestRated
+}
+
+class YelpClient: BDBOAuth1RequestOperationManager {
+    var accessToken: String!
+    var accessSecret: String!
+    
+    class var sharedInstance : YelpClient {
+        struct Static {
+            static var token : dispatch_once_t = 0
+            static var instance : YelpClient? = nil
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+        }
+        return Static.instance!
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(consumerKey key: String!, consumerSecret secret: String!, accessToken: String!, accessSecret: String!) {
+        self.accessToken = accessToken
+        self.accessSecret = accessSecret
+        let baseUrl = NSURL(string: "https://api.yelp.com/v2/")
+        super.init(baseURL: baseUrl, consumerKey: key, consumerSecret: secret);
+        
+        let token = BDBOAuth1Credential(token: accessToken, secret: accessSecret, expiration: nil)
+        self.requestSerializer.saveAccessToken(token)
+    }
+    
+    func searchWithTerm(term: String, completion: ([Business]!, NSError!) -> Void) -> AFHTTPRequestOperation {
+        return searchWithTerm(term, sort: nil, categories: nil, deals: nil, completion: completion)
+    }
+    
+    func searchWithTerm(term: String, sort: YelpSortMode?, categories: [String]?, deals: Bool?, completion: ([Business]!, NSError!) -> Void) -> AFHTTPRequestOperation {
+        // For additional parameters, see http://www.yelp.com/developers/documentation/v2/search_api
+        
+        // TODO: We need to change the ll to the hackillinois
+        var parameters: [String : AnyObject] = ["term": term, "ll": "40.107632, -88.2502165"]
+        
+        if sort != nil {
+            parameters["sort"] = sort!.rawValue
+        }
+        
+        if categories != nil && categories!.count > 0 {
+            parameters["category_filter"] = (categories!).joinWithSeparator(",")
+        }
+        
+        if deals != nil {
+            parameters["deals_filter"] = deals!
+        }
+        
+        print(parameters)
+        
+        return self.GET("search", parameters: parameters, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            let dictionaries = response["businesses"] as? [NSDictionary]
+            if dictionaries != nil {
+                completion(Business.businesses(array: dictionaries!), nil)
+            }
+            }, failure: { (operation: AFHTTPRequestOperation?, error: NSError!) -> Void in
+                completion(nil, error)
+        })!
+    }
+}
