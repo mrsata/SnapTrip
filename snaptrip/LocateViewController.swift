3ba633539e2ae4043396f82fbbaf98ae744a6c3a
MrSATA <liuhanrick@163.com>
2016-02-20 21:33:52 -0600

    Update App Structure
    add OriginViewController

--- snaptrip/LocateViewController.swift
+++ snaptrip/LocateViewController.swift
@@ -7,58 +7,37 @@
 //
 
 import UIKit
-import MapKit
-import CoreLocation
 
-
-class LocateViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
+class LocateViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // Declare the location manager
-    var locationManager:CLLocationManager?
+    
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet var mapView: MKMapView!
-    
-    var userLocation: CLLocation?
-    var locManager = CLLocationManager()
-    var currentLocation: CLLocation?
-    var NSlatitude: NSString = ""
-    var NSlongtitude: NSString = ""
-    var latitude: Float!
-    var longtitude: Float!
-    
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationManager?.delegate = self
-        locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        locationManager?.distanceFilter = 50; //meters
-        locationManager?.startUpdatingLocation()
-        locationManager?.requestAlwaysAuthorization()
         
-        // Do any additional setup after loading the view.
-        if (CLLocationManager.authorizationStatus() == .Denied) {
-            print("Authorization denied!");
-            // Check if it is denied
-        }
-
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
         self.tableView.estimatedRowHeight = 100.0;
         self.tableView.rowHeight = UITableViewAutomaticDimension;
-                
-        locManager.requestWhenInUseAuthorization()
-        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse ||
-            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedAlways){
-                currentLocation = locManager.location
-        }
-        userLocation = mapView.userLocation.location!;
-        NSlatitude = "\(userLocation!.coordinate.latitude)";
-        NSlongtitude = "\(userLocation!.coordinate.longitude)";
-        latitude = NSlatitude.floatValue
-        longtitude = NSlongtitude.floatValue
+//                
+//        locManager.requestWhenInUseAuthorization()
+//        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse ||
+//            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedAlways){
+//                currentLocation = locManager.location
+//        }
+//        userLocation = mapView.userLocation.location!;
+//        NSlatitude = "\(userLocation!.coordinate.latitude)";
+//        NSlongtitude = "\(userLocation!.coordinate.longitude)";
+//        latitude = NSlatitude.floatValue
+//        longtitude = NSlongtitude.floatValue
+        
+        
         
     }
 
@@ -94,21 +73,5 @@ class LocateViewController: UIViewController, UITableViewDataSource, UITableView
     }
     */
     
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if (locations.count > 0) {
-            for loc in locations {
-                print("Location: \(loc)")
-            }
-            
-            // print the last one only
-        }
-        print("The numbers in locations: \(locations.count)");
-    }
-    
-    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        print("LocationManager failed!")
-    }
-    
-
     
 }
---
+++ snaptrip/OriginViewController.swift
@@ -0,0 +1,84 @@
+//
+//  OriginViewController.swift
+//  SnapTrip
+//
+//  Created by LH on 2/20/16.
+//  Copyright © 2016 mrsata. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class OriginViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager:CLLocationManager?
+    
+    var userLocation: CLLocation?
+    var locManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var NSlatitude: NSString = ""
+    var NSlongtitude: NSString = ""
+    var latitude: Float!
+    var longtitude: Float!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager?.distanceFilter = 50; //meters
+        locationManager?.startUpdatingLocation()
+        locationManager?.requestAlwaysAuthorization()
+        
+        // Do any additional setup after loading the view.
+        if (CLLocationManager.authorizationStatus() == .Denied) {
+            print("Authorization denied!");
+            // Check if it is denied
+        }
+        
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        let regionRadius: CLLocationDistance = 1000
+        func centerMapOnLocation(location: CLLocation) {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                regionRadius * 2.0, regionRadius * 2.0)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+        centerMapOnLocation(initialLocation)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if (locations.count > 0) {
+            for loc in locations {
+                print("Location: \(loc)")
+            }
+            
+            // print the last one only
+        }
+        print("The numbers in locations: \(locations.count)");
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("LocationManager failed!")
+    }
+
+}
